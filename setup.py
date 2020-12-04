58f4b43c6f1b444f084f0bd6a644c67bf0d06524
Brian Tate <brian.tate@cvisionai.com>
2020-12-04 12:06:48 -0500

    Generic skeleton for tracker ruleset pip package

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='tracking_classification',
+    version='0.0.1',
+    url='https://github.com/openem-team/openem',
+    author='CVision AI',
+    author_email='info@cvisionai.com',
+    maintainer='CVision AI',
+    maintainer_email='info@cvisionai.com',
+    packages=['rules'],
+    #install_requires=reqs.readlines(),
+)
