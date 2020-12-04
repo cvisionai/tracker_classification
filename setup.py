26eba2c76163d819d89c4da8249b717d2d8a2c53
Brian Tate <brian.tate@cvisionai.com>
2020-12-04 12:42:06 -0500

    Refactor repo a bit

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    name='tracking_classification',
+    name='tracker_classification',
     version='0.0.1',
     url='https://github.com/openem-team/openem',
     author='CVision AI',
     author_email='info@cvisionai.com',
     maintainer='CVision AI',
     maintainer_email='info@cvisionai.com',
-    packages=['rules'],
+    packages=['tracker_rules'],
     #install_requires=reqs.readlines(),
 )
